f62b90ca026fb34089fb97c57a4d8a8a2aada459
Sashagelik <alek.gilyashov@yandex.ru>
2022-12-22 11:58:36 +0500

    Lending 2.0

--- js/animations.js
+++ js/animations.js
@@ -59,4 +59,26 @@ function updateScroll () {
 
     window.addEventListener('scroll', updateScroll);
 
+    function addSmoothScroll(ancore) {
+      ancore.addEventListener('click', function (event) {
+          event.preventDefault();
 
+          document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+          })
+      })   
+    }
+
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      addSmoothScroll(anchor);
+    });
+
+
+    function onLinkClick(event) {
+      event.preventDefault();
+      document.querySelector(event.target.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      })
+    }
+
+    addSmoothScroll(document.querySelector('.more-button')) 
